b209fb63c04625dd3946df587c3f11fe78c04e20
huangxy <781662424@qq.com>
2023-10-17 11:08:22 +0800

    构建library

--- webpackLibrary/src/index.js
+++ webpackLibrary/src/index.js
@@ -1,3 +1,4 @@
 import _ from "lodash";
+import './index.css'
 export const add = (a, b) => a + b
 export const max = _.max;
--- webpackLibrary/webpack.config.js
+++ webpackLibrary/webpack.config.js
@@ -1,13 +1,36 @@
-const path=require('path')
-module.exports={
-    mode:'development',
-    entry:'./src/index.js',
-    output:{
-        filename:"[name].js",
-        path:path.join(__dirname,'./dist'),
-        library:{
-            name:'_',
-            type:'umd'
+const path = require("path");
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin=require('mini-css-extract-plugin')
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./dist"),
+    library: {
+      name: "_",
+      type: "umd",
+    },
+  },
+  devtool: 'source-map',
+  module:{
+    rules:[
+        {
+            test:/\.css$/i,
+            use:[MiniCssExtractPlugin.loader,"css-loader"]
         }
-    }
-}
+    ]
+  },
+
+  plugins:[new MiniCssExtractPlugin()],
+//   排除所有node_modules模块
+  externals:[new nodeExternals()]
+//   externals: {
+//     lodash: {
+//       commonjs: "lodash",
+//       commonjs2: "lodash",
+//       amd: "lodash",
+//       root: "_",
+//     },
+//   },
+};
